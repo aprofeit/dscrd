17f0e678e7a0d55eb77bf063566ca7f185dcacb0
Alexander Profeit <alex@profeit.com>
2020-08-01 08:40:59 -0400

    fix style

--- app/javascript/packs/pages/home.jsx
+++ app/javascript/packs/pages/home.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { Col, Container, Form, InputGroup, Row } from 'react-bootstrap';
 
 const Home = () => {
