06c02b9a02ace834f39b12443babad8c5809957d
Alexander Profeit <alex@profeit.com>
2020-08-01 07:44:07 -0400

    add hmr

--- app/javascript/packs/app.jsx
+++ app/javascript/packs/app.jsx
@@ -9,17 +9,9 @@ const Hello = props => (
   <div>Hello {props.name}!</div>
 )
 
-Hello.defaultProps = {
-  name: 'David'
-}
-
-Hello.propTypes = {
-  name: PropTypes.string
-}
-
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="React" />,
+    <Hello name="Hello, world!" />,
     document.body.appendChild(document.createElement('div')),
   )
 })
