a2b7b0f9e77d411ed6aca63ae3bcfef2fb6cb5a7
Alexander Profeit <alex@profeit.com>
2020-08-01 09:08:03 -0400

    mostly finish front end

--- app/javascript/packs/pages/home.jsx
+++ app/javascript/packs/pages/home.jsx
@@ -1,19 +1,26 @@
 import React, { useState } from 'react'
-import { Col, Container, Form, InputGroup, Row } from 'react-bootstrap';
+import { Button, Col, Container, Form, InputGroup, Row } from 'react-bootstrap';
 
 const Home = () => {
   const [vanityUrl, setVanityUrl] = useState("");
+  const [destinationUrl, setDestinationUrl] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(vanityUrl)
+    console.log(destinationUrl)
+  }
 
   return (
     <Container>
       <Row>
         <Col>
-        <h1 className="mt-4">Make discord URLS great again</h1>
+        <h1 className="mt-4">Shorten your discord link</h1>
         </Col>
       </Row>
       <Row>
         <Col>
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group controlId="vanity_url">
               <Form.Label>Desired vanity URL</Form.Label>
               <InputGroup>
@@ -23,12 +30,34 @@ const Home = () => {
                 <Form.Control
                   type="text"
                   placeholder="whatever-you-want"
-                  name="vanity_url"
-                  value={vanityUrl}
-                  onChange={(_e, value) => { setVanityUrl(value) }}
+                  name="redirect[vanity_url]"
+                  onChange={e => {
+                    const newValue = e.target.value
+                    setVanityUrl(newValue)
+                  }}
                 />
               </InputGroup>
             </Form.Group>
+
+            <Form.Group controlId="destination_url">
+              <Form.Label>Destination URL</Form.Label>
+              <InputGroup>
+                <InputGroup.Prepend>
+                  <InputGroup.Text>https://discord.gg/</InputGroup.Text>
+                </InputGroup.Prepend>
+                <Form.Control
+                  type="text"
+                  name="redirect[destination_url]"
+                  onChange={e => {
+                    const newValue = e.target.value
+                    setDestinationUrl(newValue) 
+                 }}
+                />
+              </InputGroup>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Create
+            </Button>
           </Form>
         </Col>
       </Row>
--- test/models/redirect_test.rb
+++ test/models/redirect_test.rb
@@ -8,4 +8,13 @@ class RedirectTest < ActiveSupport::TestCase
   test "formatted title works as expected" do
     assert_equal "https://discord.gg/AWefEww", @nightman.formatted_url
   end
+
+  test "creating a weird redirect" do
+    assert_difference 'Redirect.count' do
+      Redirect.create!(
+        vanity_url: 'this-is-something-weird',
+        destination_url: 'https://discord.gg/ajfdslkf'
+      )
+    end
+  end
 end
