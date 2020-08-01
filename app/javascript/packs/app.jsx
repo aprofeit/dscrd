99a8e90f34f02f8d4abfc052ddfa4def18e320c3
Alexander Profeit <alex@profeit.com>
2020-08-01 08:05:34 -0400

    create initial react app

--- app/javascript/packs/app.jsx
+++ app/javascript/packs/app.jsx
@@ -4,14 +4,15 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Home from './pages/home'
 
-const Hello = props => (
-  <div>Hello {props.name}!</div>
+const App = (
+  <Home />
 )
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <Hello name="Hello, world!" />,
+    <Home />,
     document.body.appendChild(document.createElement('div')),
   )
 })
---
+++ app/javascript/packs/pages/home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container } from 'react-bootstrap';
+
+const Home = () => {
+  return (
+    <Container>
+      <h1 className="mt-4">DSCRD is on its way!</h1>
+    </Container>
+  )
+}
+
+export default Home
